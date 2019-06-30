4aba9cf55a4b6cb16ff0619b061edade3c87ac99
BayAlper10 <dursunalper1@gmail.com>
2019-06-30 16:45:58 +0300

    healthbar plugin NukkitX

---
+++ src/main/java/bayalper10/healthbar/Health.java
@@ -0,0 +1,31 @@
+package bayalper10.healthbar;
+
+import cn.nukkit.Player;
+import cn.nukkit.plugin.PluginBase;
+import cn.nukkit.scheduler.NukkitRunnable;
+
+public class Health extends PluginBase{
+
+  public void onEnable(){
+    getServer().getScheduler().scheduleDelayedRepeatingTask(this, new NukkitRunnable(){
+
+      @Override
+      public void run(){
+        for(Player p : getServer().getOnlinePlayers().values()){
+          StringBuilder str = new StringBuilder();
+
+          int i = 0;
+          while(i < p.getMaxHealth()){
+            if(p.getHealth() > i){
+              str.append("\u00A7a|");
+            }else{
+              str.append("\u00A7c|");
+            }
+            i++;
+          }
+          p.setScoreTag(str.toString());
+        }
+      }
+    }, 10, 10);
+  }
+}
